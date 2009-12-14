2aea6db3eb892830382aefb3809b4a2a576bc2ff
Alex Bennee <ajb@pitcairn.cambridgebroadband.com>
2009-12-14 12:57:07 +0000

    Find text area with jQuery and tag an ID to it

--- textareas.js
+++ textareas.js
@@ -1,37 +1,37 @@
 /*
  * TextArea.js
  *
- * This "content" script finds TextArea's in the DOM and wraps them in
- * a unique <div> block with and edit button. When the button is
+ * This "content" script finds TextArea's in the DOM and tags them
+ * with a unique ID and edit button. When the button is
  * clicked it communicates with the master extension page to send an
  * edit request.
  *
  * This uses jQuery for handy stuff
  */
+ 
+var editImgURL = chrome.extension.getURL("gumdrop.png");
+var editImgTag = "<img src=\""+editImgURL+"\">";
 
-
+function editTextArea(edit_id) {
+    console.log("editTextArea");
+    
+    alert ("Editing "+edit_id);
+}
+ 
 function findTextAreas() {
 
     console.log("Finding text area");
 
-    var result = null;
-
-    // Find the body text area
-    result = document.getElementsByTagName('textarea');
-  
-    if(result) {
-	console.log("Found some areas:"+result);
-	for (ta in result)
-	{
-	    console.log("Processing one:"+ta);
-	}
-    }
-
+    text_id=0;
+    
     // Try the jQuery way
-    result = $('textarea');
-    if (result) {
-	console.log("Found some via jQuery:"+result);
-    }
+    $('textarea').each ( function() {
+	console.log("each="+$(this));
+	id_string="eta_"+text_id;
+	$(this).attr({ emacs_chrome_id: id_string });
+	$(this).after('<a href="javascript:editTextArea(\''+id_string+'\')">'+editImgTag+'</a>');
+	text_id++;
+    } )
 	    
 }
 
