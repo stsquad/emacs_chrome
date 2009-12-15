10748750771c9691aacadf28706e92c20cc01e58
Alex Bennee <ajb@pitcairn.cambridgebroadband.com>
2009-12-15 14:53:50 +0000

    Basic plumbing to recieve edit messages

--- xmlcomms.js
+++ xmlcomms.js
@@ -4,11 +4,26 @@
  * This handles making XMLHttp calls to the remote end
  *
  */
-
  
   // Called when the user clicks on the browser action.
   chrome.browserAction.onClicked.addListener(function(tab) {
     console.log("Thingy clicked!");
-    //    chrome.tabs.executeScript(tab.id, {file: "update.js", allFrames: false});
+    chrome.tabs.executeScript(tab.id, {file: "textareas.js", allFrames: false});
     console.log("Run update");
   });
+
+
+  function handleContentMessages(msg)
+  {
+      console.log("handleContentMessages called:"+msg);
+  }
+
+  function contentTalking(port)
+  {
+      console.log("contentTalking:"+port);
+      port.onMessage.addListener(handleContentMessages);
+  }
+
+  // Hook up whenever someone connects to the extension comms port
+  chrome.extension.onConnect.addListener(contentTalking);
+
