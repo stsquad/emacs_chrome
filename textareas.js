3fb2429a3a8aa99dd369c5bff91a141a35425cdd
gfxmonk <tim3d.junk@gmail.com>
2010-11-09 21:53:25 +1100

    Made indentation consistent across all source files
    Raw tabs, displayed with indent 2. Added a section to the README
    explaining this, and added emacs & vim modelines to adhere to these
    settings.

--- textareas.js
+++ textareas.js
@@ -1,3 +1,4 @@
+// -*- tab-width:2; indent-tabs-mode:t -*- vim: set noet ts=2:
 /*
  * TextAreas.js
  *
@@ -22,144 +23,144 @@ var enable_dblclick = false;
 var enable_keys = false;
 
 /*
-  getTitle
+	getTitle
 
-  Get the title from the DOM, if that fails try to synthesise something sensible
+	Get the title from the DOM, if that fails try to synthesise something sensible
 */
 
 function getTitle()
 {
-    var title = document.title;
-    if (title == null || title.length==0) {
-	try {
-	    title = document.getElementsByTagName('title').item().innerText;
-	} catch (err) {
-	    console.log ("failed to extract title from HTML ("+err+")");
-	    title = document.documentURI;
+	var title = document.title;
+	if (title == null || title.length==0) {
+		try {
+			title = document.getElementsByTagName('title').item().innerText;
+		} catch (err) {
+			console.log ("failed to extract title from HTML ("+err+")");
+			title = document.documentURI;
+		}
 	}
-    }
-    return title;
+	return title;
 }
 
 /*
-  textAreaTracker
+	textAreaTracker
 
-  This object wraps up all the information about a given text area on the page.
-  It allows us to update the listeners and query stuff from one central location.
+	This object wraps up all the information about a given text area on the page.
+	It allows us to update the listeners and query stuff from one central location.
 */
 
 function textAreaTracker(text)
 {
-    this.edit_id = "eta_"+page_edit_id;
-    page_edit_id = page_edit_id + 1;
-    this.text = text;
-    this.text.setAttribute("edit_id", this.edit_id);
+	this.edit_id = "eta_"+page_edit_id;
+	page_edit_id = page_edit_id + 1;
+	this.text = text;
+	this.text.setAttribute("edit_id", this.edit_id);
 
-    // The text areas event handlers we attach
-    this.focusListener = setFocused;
-    this.dblclickListener = function(){sendTextArea(this);};
-    this.keydownListener = function (e) {
-	// Alt-Enter
-	if (e.altKey && e.keyCode == 13)
-	    sendTextArea(this);
-    };
-    
-    this.text.addEventListener('focus',  this.focusListener);
+	// The text areas event handlers we attach
+	this.focusListener = setFocused;
+	this.dblclickListener = function(){sendTextArea(this);};
+	this.keydownListener = function (e) {
+		// Alt-Enter
+		if (e.altKey && e.keyCode == 13)
+			sendTextArea(this);
+	};
+	
+	this.text.addEventListener('focus',  this.focusListener);
 
-    if (enable_dblclick)
-	this.text.addEventListener('dblclick', this.dblclickListener);
-    if (enable_keys)
-	this.text.addEventListener('keydown', this.keydownListener);
+	if (enable_dblclick)
+		this.text.addEventListener('dblclick', this.dblclickListener);
+	if (enable_keys)
+		this.text.addEventListener('keydown', this.keydownListener);
 
-    // The img 
-    if (enable_button) {
-	this.image = document.createElement('img');
-	this.image.style.cursor='pointer';
-	this.image.setAttribute("edit_id", this.edit_id);
-	this.image.src = editImgURL;
+	// The img 
+	if (enable_button) {
+		this.image = document.createElement('img');
+		this.image.style.cursor='pointer';
+		this.image.setAttribute("edit_id", this.edit_id);
+		this.image.src = editImgURL;
 
-	this.clickListener = editTextArea;
-	this.image.addEventListener('click', this.clickListener, false);
+		this.clickListener = editTextArea;
+		this.image.addEventListener('click', this.clickListener, false);
 
-	this.text.parentNode.insertBefore(this.image, text.nextSibling);
-    }
+		this.text.parentNode.insertBefore(this.image, text.nextSibling);
+	}
 
 }
 
 /*
-  getTextAreaTracker
+	getTextAreaTracker
 
-  Fetch the appropriate textAreaTracker for the given ID. As it tracks each element we
-  care about it's probably worth going through this short array rather than iterating
-  the entire DOM every time
+	Fetch the appropriate textAreaTracker for the given ID. As it tracks each element we
+	care about it's probably worth going through this short array rather than iterating
+	the entire DOM every time
 */
-  
+	
 function getTextAreaTracker(search_id)
 {
-    for (var i=0; i<pageTextAreas.length; i++) {
-	if (pageTextAreas[i].edit_id == search_id)
-	    return pageTextAreas[i];
-    }
+	for (var i=0; i<pageTextAreas.length; i++) {
+		if (pageTextAreas[i].edit_id == search_id)
+			return pageTextAreas[i];
+	}
 
-    return null;
+	return null;
 }
-    
+	
 /*
-  tagTextArea
+	tagTextArea
 
-  Any textarea we edit needs to be tagged with a unique ID so
-  when the get the edit response we know what to fill in. This
-  gets called several times not least as some text boxes can
-  appear in the document after first load.
+	Any textarea we edit needs to be tagged with a unique ID so
+	when the get the edit response we know what to fill in. This
+	gets called several times not least as some text boxes can
+	appear in the document after first load.
 */
 function tagTextArea(text)
 {
-    // We don't want to tag all text boxen, especially if they are hidden
-    var display = text.style.getPropertyCSSValue('display');
-    if (display && display.cssText=="none")
-    {
-	return;
-    }
-
-    // If spellcheck is turned off, usually it's just for quick editing, e.g. To: fields in gmail
-    var spellcheck = text.getAttribute("spellcheck");
-    if (spellcheck && spellcheck == "false")
-	return;
-
-    var existing_id = text.getAttribute("edit_id");
-    if (!existing_id)
-    {
-	// tag it
-	pageTextAreas.push(new textAreaTracker(text));
-    } else {
-	// Even though this text has an edit_id it might not actually be the
-	// text we think it is. If the textAreaTracker.text doesn't match then
-	// we should tag this with something different
-
-	var existing_area = getTextAreaTracker(existing_id);
-	if ( existing_area &&
-	     (existing_area.text != text ) )
+	// We don't want to tag all text boxen, especially if they are hidden
+	var display = text.style.getPropertyCSSValue('display');
+	if (display && display.cssText=="none")
 	{
-	    console.log("tagTextArea: Working around a duplicate id!");
-	    // OK, first things first, find any images that think
-	    // they are associated with a text area and remove them
-	    siblings = text.parentElement.childNodes;
-
-	    for (var j=0; j<siblings.length; j++) {
-		if (! (siblings[j].getAttribute == undefined) ) {
-		    if ( (siblings[j].getAttribute("edit_id") == existing_area.edit_id) &&
-			 (siblings[j].toString() == "[object HTMLImageElement]") ) {
-			siblings[j].parentElement.removeChild(siblings[j]);
-		    }
-		}
-	    }
-		
-	    // And create a new tracked text area
-	    pageTextAreas.push(new textAreaTracker(text));
+		return;
 	}
-    }
+
+	// If spellcheck is turned off, usually it's just for quick editing, e.g. To: fields in gmail
+	var spellcheck = text.getAttribute("spellcheck");
+	if (spellcheck && spellcheck == "false")
+		return;
+
+	var existing_id = text.getAttribute("edit_id");
+	if (!existing_id)
+	{
+		// tag it
+		pageTextAreas.push(new textAreaTracker(text));
+	} else {
+		// Even though this text has an edit_id it might not actually be the
+		// text we think it is. If the textAreaTracker.text doesn't match then
+		// we should tag this with something different
+
+		var existing_area = getTextAreaTracker(existing_id);
+		if ( existing_area &&
+			 (existing_area.text != text ) )
+		{
+			console.log("tagTextArea: Working around a duplicate id!");
+			// OK, first things first, find any images that think
+			// they are associated with a text area and remove them
+			siblings = text.parentElement.childNodes;
+
+			for (var j=0; j<siblings.length; j++) {
+				if (! (siblings[j].getAttribute == undefined) ) {
+					if ( (siblings[j].getAttribute("edit_id") == existing_area.edit_id) &&
+						 (siblings[j].toString() == "[object HTMLImageElement]") ) {
+						siblings[j].parentElement.removeChild(siblings[j]);
+					}
+				}
+			}
+				
+			// And create a new tracked text area
+			pageTextAreas.push(new textAreaTracker(text));
+		}
+	}
 }
-  
+	
 
 /*
  updateTextArea
@@ -167,68 +168,68 @@ function tagTextArea(text)
  Called when we want to update the text area with our updated text
 */
 function updateTextArea(id, content) {
-    var tracker = getTextAreaTracker(id);
-    if (tracker) {
-	tracker.text.value = content;
-	orig = $(tracker.text).css('background-color');
-	$(tracker.text).css({'background-color': 'yellow'});
-	// mark node as changed
-	var event = document.createEvent("HTMLEvents");
-	event.initEvent('change', true, false);
-	tracker.text.dispatchEvent(event);
+	var tracker = getTextAreaTracker(id);
+	if (tracker) {
+		tracker.text.value = content;
+		orig = $(tracker.text).css('background-color');
+		$(tracker.text).css({'background-color': 'yellow'});
+		// mark node as changed
+		var event = document.createEvent("HTMLEvents");
+		event.initEvent('change', true, false);
+		tracker.text.dispatchEvent(event);
 
-	setTimeout(function(){
-	 	$(tracker.text).animate({ 'background-color': orig }, 1000);
-	    }, 1000);
-    }
+		setTimeout(function(){
+				$(tracker.text).animate({ 'background-color': orig }, 1000);
+			}, 1000);
+	}
 }
 
 /*
-  sendTextArea
+	sendTextArea
 
-  Send the text area to the main part of the extension to be passed
-  on to the external editor. Eventually 
+	Send the text area to the main part of the extension to be passed
+	on to the external editor. Eventually 
 */
 
 function sendTextArea(text) {
-    var text_edit_id = text.getAttribute("edit_id");
-    // And spawn the request
-    var edit_msg = {
-	msg: "edit",
-	text: text.value,
-	title: getTitle(),
-	id: text_edit_id
-    };
-    port.postMessage(edit_msg);
+	var text_edit_id = text.getAttribute("edit_id");
+	// And spawn the request
+	var edit_msg = {
+		msg: "edit",
+		text: text.value,
+		title: getTitle(),
+		id: text_edit_id
+	};
+	port.postMessage(edit_msg);
 }
 
 /*
-  Handle focused text area
+	Handle focused text area
 */
 (function(){
-     var focusedEdit = null;
+	 var focusedEdit = null;
 
-     findActiveTextArea = function() {
-	 if (focusedEdit) {
-	     sendTextArea(focusedEdit);
-	 } else {
-	     var msg_text = "No textarea in focus in: "+getTitle();
-	     port.postMessage( {msg: "error", text: msg_text} );
-	 }
-     };
+	 findActiveTextArea = function() {
+		 if (focusedEdit) {
+			 sendTextArea(focusedEdit);
+		 } else {
+			 var msg_text = "No textarea in focus in: "+getTitle();
+			 port.postMessage( {msg: "error", text: msg_text} );
+		 }
+	 };
 
-     setFocused = function(){
-	 focusedEdit = this;
-	 // Update UI?
-	 var id = this.getAttribute("edit_id");
-	 if (id != undefined) {
-	     port.postMessage( {msg: "focus", id: id} );
-	     this.addEventListener('blur',  function() {
-		     port.postMessage( {msg: "focus", id: null} );
-		     this.removeEventListener('blur',arguments.callee,false);
-		 });
-	 }
-     };
+	 setFocused = function(){
+		 focusedEdit = this;
+		 // Update UI?
+		 var id = this.getAttribute("edit_id");
+		 if (id != undefined) {
+			 port.postMessage( {msg: "focus", id: id} );
+			 this.addEventListener('blur',	function() {
+					 port.postMessage( {msg: "focus", id: null} );
+					 this.removeEventListener('blur',arguments.callee,false);
+				 });
+		 }
+	 };
  })();
 
 /*
@@ -240,92 +241,92 @@ function sendTextArea(text) {
 */
  
 function editTextArea(event) {
-    var img = event.currentTarget;
-    var edit_id = img.getAttribute("edit_id");
+	var img = event.currentTarget;
+	var edit_id = img.getAttribute("edit_id");
 
-    var tracker = getTextAreaTracker(edit_id);
-    if (tracker) {
-	sendTextArea(tracker.text);
-    }
+	var tracker = getTextAreaTracker(edit_id);
+	if (tracker) {
+		sendTextArea(tracker.text);
+	}
 }
 
 /*
-  This is the main find function for searching for TEXTAREAS. It protects itself via
-  a (page) global semaphore called findingTextAreas so it doesn't call itself after
-  tagTextArea() adds new nodes to the DOM tree.
+	This is the main find function for searching for TEXTAREAS. It protects itself via
+	a (page) global semaphore called findingTextAreas so it doesn't call itself after
+	tagTextArea() adds new nodes to the DOM tree.
 */
 function findTextAreasIn(thing) {
 
-    // Whatever thing is it need to have the getElementsByTagName method
-    // or there isn't a lot we can do about it.
-    if (!thing.getElementsByTagName)
-	return;
-    
-    // Don't run through this if already finding stuff, lest we trigger events
-    if (findingTextAreas)
-	return;
+	// Whatever thing is it need to have the getElementsByTagName method
+	// or there isn't a lot we can do about it.
+	if (!thing.getElementsByTagName)
+		return;
+	
+	// Don't run through this if already finding stuff, lest we trigger events
+	if (findingTextAreas)
+		return;
 
-    findingTextAreas = true;
-    
-    try {
-	var texts = thing.getElementsByTagName('textarea');
-	for (var i=0; i<texts.length; i++) {
-	    tagTextArea(texts[i]);
+	findingTextAreas = true;
+	
+	try {
+		var texts = thing.getElementsByTagName('textarea');
+		for (var i=0; i<texts.length; i++) {
+			tagTextArea(texts[i]);
+		}
+	} catch (err) {
+		// it seems some (I)FRAMES have undefined contentDocuments
+		console.log("findTextAreasIn: failed with "+err);
 	}
-    } catch (err) {
-	// it seems some (I)FRAMES have undefined contentDocuments
-	console.log("findTextAreasIn: failed with "+err);
-    }
 
-    findingTextAreas = false;
+	findingTextAreas = false;
 }
 
 /*
-  Exhaustively search the page for textareas.
+	Exhaustively search the page for textareas.
 */
 function findTextAreas() {
 
-    // The main document
-    findTextAreasIn(document);
+	// The main document
+	findTextAreasIn(document);
 
-    // IFRAMEs
-    var iframes = document.getElementsByTagName('iframe');
-    for (i = 0; i < iframes.length; i++) {
-	findTextAreasIn(iframes[i].contentDocument);
-    }
+	// IFRAMEs
+	var iframes = document.getElementsByTagName('iframe');
+	for (i = 0; i < iframes.length; i++) {
+		findTextAreasIn(iframes[i].contentDocument);
+	}
 
-    // FRAMEs
-    var frames = document.getElementsByTagName('frame');
-    for (i = 0; i < frames.length; i++) {
-	findTextAreasIn(frames[i].contentDocument);
-    }
+	// FRAMEs
+	var frames = document.getElementsByTagName('frame');
+	for (i = 0; i < frames.length; i++) {
+		findTextAreasIn(frames[i].contentDocument);
+	}
 
-    return true;
+	return true;
 }
 
 /* Message handling multiplexer */
 function localMessageHandler(msg, port) {
-    // What was the bidding?
-    var cmd = msg.msg;
-    if (cmd == "config") {
-	console.log("config response: "+msg);
-	enable_button = msg.enable_button;
-	enable_dblclick = msg.enable_dblclick;
-	enable_keys = msg.enable_keys;
-	findTextAreas();
-	document.addEventListener("DOMNodeInserted", (function (ev) {
-		    findTextAreas();
-		    return true;
-		}), false);
-    } else if (cmd == "find_edit") {
-	findActiveTextArea();
-    } else if (cmd == "update") {
-	var id = msg.id;
-	var content = msg.text;
-	updateTextArea(id, content);
-    } else {
-	console.log("localMessageHandler: un-handled message:"+cmd);
-    }
+	// What was the bidding?
+	var cmd = msg.msg;
+	if (cmd == "config") {
+		console.log("config response: "+msg);
+		enable_button = msg.enable_button;
+		enable_dblclick = msg.enable_dblclick;
+		enable_keys = msg.enable_keys;
+		findTextAreas();
+		document.addEventListener("DOMNodeInserted", (function (ev) {
+					findTextAreas();
+					return true;
+				}), false);
+	} else if (cmd == "find_edit") {
+		findActiveTextArea();
+	} else if (cmd == "update") {
+		var id = msg.id;
+		var content = msg.text;
+		updateTextArea(id, content);
+	} else {
+		console.log("localMessageHandler: un-handled message:"+cmd);
+	}
 }
 
 // Hook up the incoming message handler for both return messages
@@ -333,12 +334,12 @@ function localMessageHandler(msg, port) {
 
 port.onMessage.addListener(localMessageHandler);
 chrome.extension.onConnect.addListener(function(iport) {
-	iport.onMessage.addListener(localMessageHandler);
-    });
+		iport.onMessage.addListener(localMessageHandler);
+	});
 
 /*
-  To start the whole process off we first need to fetch our configuration
-  from the background process.
+	To start the whole process off we first need to fetch our configuration
+	from the background process.
 */
 port.postMessage({msg: "config"});
 
@@ -347,18 +348,18 @@ port.postMessage({msg: "config"});
 // the context menu on an editable element.
 document.addEventListener("contextmenu", (function(event) {
 
-  var elem = event.srcElement;
-  if (elem && elem.getAttribute("edit_id")) {
-    var request = {
-      type: "menu_target",
-      edit_msg: {
-        msg: "edit",
-        text: elem.value,
-        title: getTitle(),
-        id: elem.getAttribute("edit_id")
-      }
-    };
-    chrome.extension.sendRequest(request);
-  }
+	var elem = event.srcElement;
+	if (elem && elem.getAttribute("edit_id")) {
+	var request = {
+		type: "menu_target",
+		edit_msg: {
+		msg: "edit",
+		text: elem.value,
+		title: getTitle(),
+		id: elem.getAttribute("edit_id")
+		}
+	};
+	chrome.extension.sendRequest(request);
+	}
 
 }));
--- xmlcomms.js
+++ xmlcomms.js
@@ -1,3 +1,4 @@
+// -*- tab-width:2; indent-tabs-mode:t -*- vim: set noet ts=2:
 /*
  * xmlcomms.js
  *
@@ -14,11 +15,11 @@
 // Get the base URL from which we make all requests to the server..
 function getEditUrl()
 {
-    var port = localStorage["edit_server_port"];
-    if (!port) {
+	var port = localStorage["edit_server_port"];
+	if (!port) {
 	port = 9292;
-    }
-    return "http://127.0.0.1:" +  port + "/";
+	}
+	return "http://127.0.0.1:" +	port + "/";
 }
 
 /*
@@ -26,38 +27,38 @@ function getEditUrl()
  */
 function updateUserFeedback(string, colour)
 {
-    console.log("updateUserFeedback: "+string);
-    chrome.browserAction.setTitle({title:string});
-    if (colour == null) {
-    	chrome.browserAction.setIcon({path:"emacs23-16x16.png"});
-    } else if (colour == "green") {
+	console.log("updateUserFeedback: "+string);
+	chrome.browserAction.setTitle({title:string});
+	if (colour == null) {
+		chrome.browserAction.setIcon({path:"emacs23-16x16.png"});
+	} else if (colour == "green") {
 	chrome.browserAction.setIcon({path:"emacs23-16x16-green.png"});
-    } else if (colour == "red") {
+	} else if (colour == "red") {
 	chrome.browserAction.setIcon({path:"emacs23-16x16-red.png"});
-    } else if (colour == "darkblue") {
+	} else if (colour == "darkblue") {
 	chrome.browserAction.setIcon({path:"emacs23-16x16-darker.png"});
-    } else {
-    	chrome.browserAction.setIcon({path:"emacs23-16x16.png"});
-    }
+	} else {
+		chrome.browserAction.setIcon({path:"emacs23-16x16.png"});
+	}
 }
-    
+	
 // Initial message
 updateUserFeedback("Awaiting edit request", "blue");
 
 // Called when the user clicks on the browser action.
-//    
+//
 // When clicked we send a message to the current active tab's
 // content script. It will then use heuristics to decide which text
 // area to spawn an edit request for.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  
-  var find_msg = {
-      msg: "find_edit"
-  };
-  var tab_port = chrome.tabs.connect(tab.id);
-  
-  tab_port.postMessage(find_msg);
-  updateUserFeedback("sent request to content script", "green");
+	
+	var find_msg = {
+		msg: "find_edit"
+	};
+	var tab_port = chrome.tabs.connect(tab.id);
+	
+	tab_port.postMessage(find_msg);
+	updateUserFeedback("sent request to content script", "green");
 });
 
 // Handle and edit request coming from the content page script
@@ -65,58 +66,58 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 // Package up the text to be edited and send it to the edit server
 function handleContentMessages(msg, tab_port)
 {
-    console.log("handleContentMessages called:"+JSON.stringify(msg));
-    var cmd = msg.msg;
-    var id = msg.id;
-    var text = msg.text;
-    var file = msg.file;
+	console.log("handleContentMessages called:"+JSON.stringify(msg));
+	var cmd = msg.msg;
+	var id = msg.id;
+	var text = msg.text;
+	var file = msg.file;
 
-    var xhr = new XMLHttpRequest();
-    var url = getEditUrl() + cmd;
+	var xhr = new XMLHttpRequest();
+	var url = getEditUrl() + cmd;
 
-    console.log(" page URL:"+tab_port.tab.url);
-    console.log(" tab_port:"+tab_port.portId_);
-    console.log(" request URL:"+url);
-    
-    xhr.open("POST", url, true);
-    
-    xhr.onreadystatechange = function() {
-        console.log("State change:"+ xhr.readyState + " status:"+xhr.status);
+	console.log(" page URL:"+tab_port.tab.url);
+	console.log(" tab_port:"+tab_port.portId_);
+	console.log(" request URL:"+url);
+	
+	xhr.open("POST", url, true);
+	
+	xhr.onreadystatechange = function() {
+		console.log("State change:"+ xhr.readyState + " status:"+xhr.status);
 	// readyState 4=HTTP response complete
-        if(xhr.readyState == 4) {
-	    if (xhr.status == 200) {
+		if(xhr.readyState == 4) {
+		if (xhr.status == 200) {
 		
 		var update_msg = {
-		    msg: "update",
-		    text: xhr.responseText,
-		    id: id
+			msg: "update",
+			text: xhr.responseText,
+			id: id
 		};
 
 		updateUserFeedback("Successful edit of "+msg.title);
 		tab_port.postMessage(update_msg);
 
-                msg.text = xhr.responseText;
-                msg.file = xhr.getResponseHeader("x-file");
-                if(xhr.getResponseHeader("x-open") == "true") {
-                    handleContentMessages(msg, tab_port);
-                }
-	    } else if (xhr.status == 0) {
+				msg.text = xhr.responseText;
+				msg.file = xhr.getResponseHeader("x-file");
+				if(xhr.getResponseHeader("x-open") == "true") {
+					handleContentMessages(msg, tab_port);
+				}
+		} else if (xhr.status == 0) {
 		// Is the edit server actually running?
 		updateUserFeedback("Error: is edit server running?", "red");
-	    } else {
+		} else {
 		updateUserFeedback("Un-handled response: "+xhr.status, "red"); 
-	    }
-        }
-    }
+		}
+		}
+	}
 
-    // reset the display before sending request..
-    updateUserFeedback("Edit request sent for "+msg.title, "green");
+	// reset the display before sending request..
+	updateUserFeedback("Edit request sent for "+msg.title, "green");
 
-    xhr.setRequestHeader("Content-type", "text/plain");
-    xhr.setRequestHeader("x-url", tab_port.tab.url);
-    xhr.setRequestHeader("x-id", id);
-    xhr.setRequestHeader("x-file", file);
-    xhr.send(text);
+	xhr.setRequestHeader("Content-type", "text/plain");
+	xhr.setRequestHeader("x-url", tab_port.tab.url);
+	xhr.setRequestHeader("x-id", id);
+	xhr.setRequestHeader("x-file", file);
+	xhr.send(text);
 }
 
 // Handle and edit request coming from the content page script
@@ -124,71 +125,71 @@ function handleContentMessages(msg, tab_port)
 // Package up the text to be edited and send it to the edit server
 function handleTestMessages(msg, tab_port)
 {
-    var url = getEditUrl() + "status";
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function() {
+	var url = getEditUrl() + "status";
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.onreadystatechange = function() {
 	console.log("State change:"+ xhr.readyState + " status:"+xhr.status);
 	// readyState 4=HTTP response complete
 	if(xhr.readyState == 4) {
-	    if (xhr.status == 200) {
+		if (xhr.status == 200) {
 		tab_port.postMessage({msg: "test_result", text: xhr.responseText});
-	    } else if (xhr.status == 0) {
+		} else if (xhr.status == 0) {
 		tab_port.postMessage({msg: "test_result", text: "Edit Server Test failed: is it running?"});
-	    } else {
+		} else {
 		tab_port.postMessage({msg: "test_result", text: "Un-handled response: "+xhr.status}); 
-	    }
+		}
 	}
-    }
-    xhr.send();
+	}
+	xhr.send();
 }
 
 // Handle config request messages, the textarea.js content script being in it's own
 // isolated sandbox has to be fed all this via the IPC mechanisms
 
 function getBooleanConfig(config, defaultval) {
-    return typeof localStorage[config] == "undefined" ? defaultval : localStorage[config] == "true";
+	return typeof localStorage[config] == "undefined" ? defaultval : localStorage[config] == "true";
 }
 
 
 function handleConfigMessages(msg, tab_port)
 {
-    var config_msg = {
+	var config_msg = {
 	msg: "config",
 	enable_button: getBooleanConfig("enable_button", true),
 	enable_dblclick: getBooleanConfig("enable_dblclick", false),
 	enable_keys: getBooleanConfig("enable_keys", false)
-    };
-    tab_port.postMessage(config_msg);
+	};
+	tab_port.postMessage(config_msg);
 }
 
 
 /*
-  Handle all in-coming messages to the extension.
+	Handle all in-coming messages to the extension.
 
-  As other parts of the extension cannot trigger XHR requests they all
-  send message to the main part of the extension to service these requests.
+	As other parts of the extension cannot trigger XHR requests they all
+	send message to the main part of the extension to service these requests.
 */
  
 function localMessageHandler(port)
 {
-    port.onMessage.addListener(function(msg, port) {
-        if (msg.msg == "config") {
-	    handleConfigMessages(msg, port);
+	port.onMessage.addListener(function(msg, port) {
+		if (msg.msg == "config") {
+		handleConfigMessages(msg, port);
 	} else if (msg.msg == "edit") {
-	    handleContentMessages(msg, port);
+		handleContentMessages(msg, port);
 	} else if (msg.msg == "test") {
-	    handleTestMessages(msg, port);
+		handleTestMessages(msg, port);
 	} else if (msg.msg == "error") {
-	    updateUserFeedback(msg.text, "red");
+		updateUserFeedback(msg.text, "red");
 	} else if (msg.msg == "focus") {
-	    if (msg.id === null) {
+		if (msg.id === null) {
 		updateUserFeedback("Awaiting edit request: no focus", "darkblue");
-	    } else {
+		} else {
 		updateUserFeedback("Awaiting edit request: in focus");
-	    }
+		}
 	}
-    });
+	});
 }
 
 // Hook up whenever someone connects to the extension comms port
