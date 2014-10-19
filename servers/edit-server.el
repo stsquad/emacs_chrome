stsquad/emacs_chrome

commit a4e1dbe1c45b41c0de99a61134323965d1f4b5bd

edit-server.el: simpler processing of edit-server-url-major-mode-alist.

servers/edit-server.el
@@ -507,15 +507,12 @@ frame. If a frame was created it returns `FRAME'."
   "Use `edit-server-url-major-mode-alist' to choose a major mode
 initialization function based on `edit-server-url', or fall back
 to `edit-server-default-major-mode'"
-  (let ((pairs edit-server-url-major-mode-alist)
-	(mode edit-server-default-major-mode))
-    (while pairs
-      (let ((entry (car pairs)))
-	(if (string-match (car entry) edit-server-url)
-	    (setq mode (cdr entry)
-		  pairs nil)
-	  (setq pairs (cdr pairs)))))
-    (funcall mode)))
+  (let ((mode
+         (assoc-default
+          edit-server-url
+          edit-server-url-major-mode-alist 'string-match)))
+    (when mode
+      (funcall mode))))
 
 (defun edit-server-find-or-create-edit-buffer(proc &optional existing)
   "Find and existing or create an new edit buffer, place content in it

commit f266b009edf82cb49d1fd3f325c17bf99e653bfb

Fix a bug in `edit-server-choose-major-mode'.
Looks like commit a4e1dbe1 mistakenly dropped the fallback to
`edit-server-default-major-mode'.  Bring it back.  No need for the test
now, since `edit-server-default-major-mode' can't be nil (according to
its declaration).

servers/edit-server.el
@@ -507,12 +507,9 @@ frame. If a frame was created it returns `FRAME'."
   "Use `edit-server-url-major-mode-alist' to choose a major mode
 initialization function based on `edit-server-url', or fall back
 to `edit-server-default-major-mode'"
-  (let ((mode
-         (assoc-default
-          edit-server-url
-          edit-server-url-major-mode-alist 'string-match)))
-    (when mode
-      (funcall mode))))
+  (funcall (or (assoc-default edit-server-url
+                              edit-server-url-major-mode-alist 'string-match)
+               edit-server-default-major-mode)))
 
 (defun edit-server-find-or-create-edit-buffer (proc &optional existing)
   "Find and existing or create an new edit buffer, place content in it