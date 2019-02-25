4697052d7ca466f33c5692bf062fa6cb8a268c07
Alex Bennée <alex.bennee@linaro.org>
2019-02-25 13:58:21 +0000

    test-travis.sh: quote 'ert-async

--- test-travis.sh
+++ test-travis.sh
@@ -6,7 +6,7 @@ set -ex
 
 # About the simplest test we can do, ensure the file loads without error
 emacs --version
-emacs -q --batch -e "(package-install ert-async)"
+emacs -q --batch -e "(package-install 'ert-async)"
 emacs -q --batch -l ert -l servers/edit-server.el -l servers/edit-server-ert.el -f ert-run-tests-batch-and-exit
 
 # How about some lint tests for the JavaScript
