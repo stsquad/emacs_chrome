beb654e50390d86067039efef58dcb6f8d014a6a
Alex Bennée <alex.bennee@linaro.org>
2019-02-25 12:13:59 +0000

    testing: add a edit-server ping test

--- servers/edit-server-ert.el
+++ servers/edit-server-ert.el
@@ -2,8 +2,10 @@
 
 ;; Copyright (C) 2013  Alex Bennée
 
+(package-initialize)
 
 (require 'ert)
+(require 'ert-async)
 
 ; This test needs edit-server loaded to find the path to the file
 (ert-deftest edit-server-compiles ()
@@ -12,3 +14,12 @@
         (edit-server-file (find-lisp-object-file-name 'edit-server-start (symbol-function 'edit-server-start))))
     (should (byte-compile-file edit-server-file))))
 
+(ert-deftest-async edit-server-responds (done)
+  "Test the edit server starts and responds to a ping test."
+  (let ((edit-server-verbose t)
+        (edit-server-port 9393))
+    (edit-server-start)
+    (url-retrieve
+     "http://localhost:9393/ping"
+     (lambda (x) (funcall done)))
+    (edit-server-stop)))
