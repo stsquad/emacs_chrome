49bb6e0b6f04a3dd840bd38e57bb206db3ef9957
Alex Bennée <alex.bennee@linaro.org>
2019-02-25 14:12:42 +0000

    tests: move prereq setup into own file
    Calling -e from shell script was fiddly and broke.

---
+++ tests/install-prereqs.el
@@ -0,0 +1,13 @@
+;;; install-prereqs --- Install any pre-requisites needed for testing
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+(package-initialize)
+(package-install 'ert-async)
+
+(provide 'install-prereqs)
+;;; install-prereqs.el ends here
